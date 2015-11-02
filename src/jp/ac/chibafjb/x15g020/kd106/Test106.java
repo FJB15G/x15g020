b7838f1d2d5a0f893d0f7bbc9440712719323cc4
unknown <x15g020@PC504N013.chiba-fjb.ac.jp>
2015-11-02 15:12:50 +0900

    106 初期

---
+++ src/jp/ac/chibafjb/x15g020/kd106/Test106.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g020.kd106;
+
+public class Test106 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("趣味　：　オロナミンC"+"aaa");
+	}
+
+}

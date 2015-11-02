de4d86562a67e0a80b79795ee735375eca54fd4b
unknown <x15g020@PC504N013.chiba-fjb.ac.jp>
2015-11-02 14:50:19 +0900

    103 初期ファイル

---
+++ src/jp/ac/chibafjb/x15g020/kd103/Test103.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g020.kd103;
+
+public class Test103 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("1しょうじこうへい 2しょうじこうへい 3しょうじこうへい 4しょうじこうへい 5しょうじこうへい 6しょうじこうへい 7しょうじこうへい 8しょうじこうへい 9しょうじこうへい 10しょうじこうへい");
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g020/kd104/Test104.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g020.kd104;
+
+public class Test104 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}

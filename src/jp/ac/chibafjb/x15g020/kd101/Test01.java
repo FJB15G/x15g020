7872550533df7d7149ac2ffe88746da2d96944a3
unknown <x15g020@PC504N013.chiba-fjb.ac.jp>
2015-11-02 14:39:59 +0900

    102 初期ファイル

--- src/jp/ac/chibafjb/x15g020/kd101/Test01.java
+++ src/jp/ac/chibafjb/x15g020/kd101/Test01.java
@@ -2,7 +2,8 @@ package jp.ac.chibafjb.x15g020.kd101;
 
 public class Test01 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO 自動成されたメソッド・スタブ
 		System.out.print("船橋情報ビジネス専門学校\n047-425-1051\nHello☆こんにちは");
 	}

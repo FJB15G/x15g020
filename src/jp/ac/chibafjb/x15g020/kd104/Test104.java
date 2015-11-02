54ce6edc5c4a98d99da5a3f3a8580c26faecfbc6
unknown <x15g020@PC504N013.chiba-fjb.ac.jp>
2015-11-02 14:51:57 +0900

    104 ファイル

--- src/jp/ac/chibafjb/x15g020/kd104/Test104.java
+++ src/jp/ac/chibafjb/x15g020/kd104/Test104.java
@@ -4,7 +4,10 @@ public class Test104 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		
 	}
 
 }

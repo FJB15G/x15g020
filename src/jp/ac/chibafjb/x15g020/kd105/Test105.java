239dc0f77e675ed8c588a37c194d174848ca2ebf
unknown <x15g020@PC504N013.chiba-fjb.ac.jp>
2015-11-02 15:03:50 +0900

    105 初期

---
+++ src/jp/ac/chibafjb/x15g020/kd105/Test105.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g020.kd105;
+
+public class Test105 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+			System.out.println("氏名：　ミンナミン　太郎（ふなばし　たろう）");
+			System.out.println("趣味　：　ミンナミンC");
+			System.out.println("趣味　：　オロナミンC");
+			
+			
+	}
+
+}
